760218a332b2fa9edfcb4b2b96581a1aee74a8ee
evf <eliseuv816@gmail.com>
2025-11-23 20:28:23 -0300

    [vscode] create module

--- home/editor/vscode.nix
+++ home/editor/vscode.nix
@@ -1,24 +1,9 @@
-{ pkgs, ... }:
+{ pkgs-unstable, ... }:
 {
 
   programs.vscode = {
-
     enable = true;
-    profiles = {
-      default = {
-        extensions = with pkgs.vscode-extensions; [
-          # Vim emulation
-          vscodevim.vim
-          # Theme
-          catppuccin.catppuccin-vsc
-          catppuccin.catppuccin-vsc-icons
-          # Copilot
-          github.copilot
-          github.copilot-chat
-        ];
-      };
-    };
-
+    package = pkgs-unstable.vscode-fhs;
   };
 
 }
