daa73faec8ff001ebead5068279452adef3bcdaf
evf <eliseuv816@gmail.com>
2025-11-30 17:28:26 -0300

    Reapply "[television] remove config"
    This reverts commit e5cbd123ee77eb7b3e41fce85c379ee4ffc3fec9.

--- home/shell/tools/television.nix
+++ home/shell/tools/television.nix
@@ -5,35 +5,6 @@
     enable = true;
     enableZshIntegration = true;
     channels = { };
-    settings = {
-      shell_integration = {
-        channel_triggers = {
-          dirs = [
-            "cd"
-            "ls"
-            "l"
-            "ll"
-          ];
-          files = [
-            "cat"
-            "bat"
-            "b"
-          ];
-        };
-      };
-      tick_rate = 50;
-      ui = {
-        use_nerd_font_icons = true;
-        theme = "tokyonight";
-        input_bar_position = "bottom";
-      };
-      keybindings = {
-        quit = [
-          "esc"
-          "ctrl-c"
-        ];
-      };
-    };
   };
 
 }
