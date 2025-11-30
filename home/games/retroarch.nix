01cb6844d104e8d88f72c905002e419f6a3e5223
evf <eliseuv@pm.me>
2025-11-30 18:11:57 -0300

    [retroarch] use home-manager module

--- home/games/retroarch.nix
+++ home/games/retroarch.nix
@@ -1,27 +1,25 @@
 { pkgs, ... }:
-let
-  retroarchWithCores = (
-    pkgs.retroarch.withCores (
-      cores: with cores; [
-
-        # GBA
-        mgba
-
-        # NES
-        nestopia
-
-        # SNES
-        snes9x2010
-
-        # N64
-        mupen64plus
-
-      ]
-    )
-  );
-in
 {
 
-  home.packages = [ retroarchWithCores ];
+  programs.retroarch = {
+    enable = true;
+    settings = {
+      video_driver = "vulkan";
+      video_fullscreen = "true";
+    };
+    cores = {
+      # GBA
+      mgba.enable = true;
+      # NES
+      nestopia.enable = true;
+      # SNES
+      snes9x = {
+        enable = true;
+        package = pkgs.libretro.snes9x2010;
+      };
+      # N64
+      mupen64plus.enable = true;
+    };
+  };
 
 }
