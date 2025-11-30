8333ccc1748ada4868017486f0c899ffba2efb53
evf <eliseuv816@gmail.com>
2025-11-30 17:13:06 -0300

    [flake] NixOS 25.05 -> 25.11

--- flake.nix
+++ flake.nix
@@ -18,12 +18,12 @@
 
   inputs = {
 
-    nixpkgs.url = "nixpkgs/nixos-25.05";
+    nixpkgs.url = "nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
 
     # Home Manager
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
