57b6f524052669dc96d2ff15bd97f715d9daf5d4
evf <eliseuv@pm.me>
2025-11-30 17:44:04 -0300

    [git] update config options

--- home/development/git/git.nix
+++ home/development/git/git.nix
@@ -5,10 +5,12 @@
 
     enable = true;
 
-    userName = "evf";
-    userEmail = "eliseuv816@gmail.com";
+    settings = {
 
-    extraConfig = {
+      user = {
+        name = "evf";
+        email = "eliseuv@pm.me";
+      };
 
       core = {
         editor = "nvim";
@@ -29,29 +31,30 @@
       repositories = [ "/home/evf/dotfiles" ];
     };
 
-    delta = {
-      enable = true;
-      options = {
-        features = "decorations";
-        navigate = true; # use n and N to move between diff sections
-        light = false; # set to true if you're in a terminal w/ a light background color (e.g. the default macOS terminal)
-        line-numbers = true;
-        side-by-side = false;
-        interactive = {
-          keep-plus-minus-markers = false;
-        };
-        decorations = {
-          commit-decoration-style = "blue ol";
-          commit-style = "raw";
-          file-style = "omit";
-          hunk-header-decoration-style = "blue box";
-          hunk-header-file-style = "red";
-          hunk-header-line-number-style = "#067a00";
-          hunk-header-style = "file line-number syntax";
-        };
+  };
+
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+    options = {
+      features = "decorations";
+      navigate = true; # use n and N to move between diff sections
+      light = false; # set to true if you're in a terminal w/ a light background color (e.g. the default macOS terminal)
+      line-numbers = true;
+      side-by-side = false;
+      interactive = {
+        keep-plus-minus-markers = false;
+      };
+      decorations = {
+        commit-decoration-style = "blue ol";
+        commit-style = "raw";
+        file-style = "omit";
+        hunk-header-decoration-style = "blue box";
+        hunk-header-file-style = "red";
+        hunk-header-line-number-style = "#067a00";
+        hunk-header-style = "file line-number syntax";
       };
     };
-
   };
 
   home.shellAliases = {
