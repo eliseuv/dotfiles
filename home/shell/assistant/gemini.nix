9e329d0eb25605a1c1ac3f1918e55236d6c47a02
evf <eliseuv@pm.me>
2025-12-08 10:43:59 -0300

    [gemini] use only nixpkgs unstable

--- home/shell/assistant/gemini.nix
+++ home/shell/assistant/gemini.nix
@@ -1,19 +1,19 @@
 {
-  pkgs-unstable,
+  pkgs,
   lib,
   ...
 }:
 let
-  gemini-cli-override = pkgs-unstable.gemini-cli.overrideAttrs (old: rec {
+  gemini-cli-override = pkgs.gemini-cli.overrideAttrs (old: rec {
     version = "0.19.4";
-    src = pkgs-unstable.fetchFromGitHub {
+    src = pkgs.fetchFromGitHub {
       owner = "google-gemini";
       repo = "gemini-cli";
       tag = "v${version}";
       hash = "sha256-bXolK7TEfrmSuntE0uP6MpLNypgyqjaL85bbPi19T5k=";
     };
     npmDepsHash = "sha256-sE3dTngvVukcL7Cwm5MG1NVCBGDbSW2YrkJyGEbg2Ow=";
-    npmDeps = pkgs-unstable.fetchNpmDeps {
+    npmDeps = pkgs.fetchNpmDeps {
       inherit src;
       name = "${old.pname}-${version}-npm-deps";
       hash = npmDepsHash;
