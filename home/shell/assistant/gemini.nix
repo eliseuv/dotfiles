f84218a1b6da84cc0f479c677cb4b22c5fec7769
evf <eliseuv@pm.me>
2025-12-17 22:36:28 -0300

    [gemini] use gemini-cli-bin from nixpkgs

--- home/shell/assistant/gemini.nix
+++ home/shell/assistant/gemini.nix
@@ -1,30 +1,11 @@
 {
   pkgs,
-  lib,
   ...
 }:
-let
-  gemini-cli-override = pkgs.gemini-cli.overrideAttrs (old: rec {
-    version = "0.19.4";
-    src = pkgs.fetchFromGitHub {
-      owner = "google-gemini";
-      repo = "gemini-cli";
-      tag = "v${version}";
-      hash = "sha256-bXolK7TEfrmSuntE0uP6MpLNypgyqjaL85bbPi19T5k=";
-    };
-    npmDepsHash = "sha256-sE3dTngvVukcL7Cwm5MG1NVCBGDbSW2YrkJyGEbg2Ow=";
-    npmDeps = pkgs.fetchNpmDeps {
-      inherit src;
-      name = "${old.pname}-${version}-npm-deps";
-      hash = npmDepsHash;
-    };
-  });
-in
 {
-  home.packages = [
+  home.packages = with pkgs; [
 
-    # Custom version of Gemini CLI
-    gemini-cli-override
+    gemini-cli-bin
 
   ];
 }
