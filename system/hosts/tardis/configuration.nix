6acdeb01328c156f46f7580f0f507028398dfb9f
evf <eliseuv@pm.me>
2026-01-10 13:10:00 -0300

    Revert "[GLaDOS] remove wm config from host"
    This reverts commit 62c87a9e0c3fd8b81415c1d1761e434a2e683ed6.

--- system/hosts/tardis/configuration.nix
+++ system/hosts/tardis/configuration.nix
@@ -35,6 +35,10 @@
     # Display manager
     ../../desktop/display-manager/gdm/default.nix
 
+    # Window manager
+    ../../desktop/window-manager/hyprland.nix
+    ../../desktop/window-manager/gnome.nix
+
     # Boot graphics
     ../../extra/plymouth.nix
 
@@ -53,6 +57,9 @@
 
   # Allow user to install system-wide packages
   nix.settings.trusted-users = [ "evf" ];
+
+  # Select default session for Display Manager
+  services.displayManager.defaultSession = "hyprland";
 
   # Disk encryption
   boot.initrd.luks.devices."luks-2ac9cd27-6ff4-4407-9808-c63a5251c44c".device =
