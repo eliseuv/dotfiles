c5f4264758f94027b436ebfd4d20194fd71f4207
evf <eliseuv@pm.me>
2026-01-19 14:45:22 -0300

    [hyprland] use cattpuccin theme

--- home/desktop/window-manager/hyprland/default.nix
+++ home/desktop/window-manager/hyprland/default.nix
@@ -8,20 +8,19 @@
 
   ];
 
+  # Hyprland config
+  home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
+
+  # Catppuccin theme
+  catppuccin.hyprland = {
+    enable = true;
+  };
+
   # Default terminal
   programs.kitty.enable = true;
 
   # Clipboard manager
   services.copyq.enable = true;
-
-  # Hyprland config
-  home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
-
-  # Catppuccin theme
-  home.file.".config/hypr/catppuccin" = {
-    source = ./themes/catppuccin;
-    recursive = true;
-  };
 
   # Screenshot
   programs.hyprshot = {
