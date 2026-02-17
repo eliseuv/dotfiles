4589c3e274a8878b5e34fd77c4fe34fc2f850a05
evf <eliseuv@pm.me>
2026-02-17 15:38:10 -0300

    [maintenance] disable nix.gc to avoid conflict with programs.nh.clean

--- system/environment/maintenance.nix
+++ system/environment/maintenance.nix
@@ -17,7 +17,7 @@
 
   # Periodic garbage collection
   nix.gc = {
-    automatic = true;
+    automatic = false;
     dates = "weekly";
     options = "--delete-older-than 7d";
     persistent = true;
