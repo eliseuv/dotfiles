fb4bd02ed615ccfa755b4ca941f6c9025c3d5b20
evf <eliseuv@pm.me>
2026-03-28 11:22:27 -0300

    [antigravity] stop syncing extensions

--- home/editor/antigravity/link.sh
+++ home/editor/antigravity/link.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
 ln --symbolic "$(realpath ./settings.json)" ~/.config/Antigravity/User/settings.json
-
-ln --symbolic "$(realpath ./extensions.json)" ~/.antigravity/extensions/extensions.json
