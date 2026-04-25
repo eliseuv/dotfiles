bbe2a2a2424711369573382e7b7163989d097302
evf <eliseuv@pm.me>
2026-04-25 18:22:41 -0300

    [firefox] explicit new config path
    Also had to move the contents of ~/.mozilla/firefox to
    ~/.config/mozilla/firefox

--- home/browser/firefox/default.nix
+++ home/browser/firefox/default.nix
@@ -1,9 +1,10 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
 
   programs.firefox = {
     enable = true;
     nativeMessagingHosts = [ pkgs.tridactyl-native ];
+    configPath = "${config.xdg.configHome}/mozilla/firefox";
     profiles = {
       "evf" = {
         userChrome = builtins.readFile ./userChrome.css;
