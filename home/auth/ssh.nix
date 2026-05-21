6f48545afd5fcb50aa9fe2af170c7c65081dfe01
evf <eliseuv@pm.me>
2026-05-20 23:17:43 -0300

    [ssh] matchblocks -> settings

--- home/auth/ssh.nix
+++ home/auth/ssh.nix
@@ -4,7 +4,7 @@
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
-    matchBlocks = {
+    settings = {
       # Default settings for all hosts
       "*" = {
         compression = true;
