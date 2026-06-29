cfee58288ed536e45ed325efd777ddc7c55cf0f1
evf <eliseuv@pm.me>
2026-06-29 20:37:28 -0300

    [ttyd] apply Catppuccin Mocha theme via Nix attribute set

--- home/extra/ttyd.nix
+++ home/extra/ttyd.nix
@@ -1,4 +1,29 @@
 { pkgs, config, ... }:
+let
+  theme = {
+    background = "#1e1e2e";
+    foreground = "#cdd6f4";
+    cursor = "#f5e0dc";
+    cursorAccent = "#1e1e2e";
+    selection = "#585b70";
+    black = "#45475a";
+    red = "#f38ba8";
+    green = "#a6e3a1";
+    yellow = "#f9e2af";
+    blue = "#89b4fa";
+    magenta = "#f5c2e7";
+    cyan = "#94e2d5";
+    white = "#bac2de";
+    brightBlack = "#585b70";
+    brightRed = "#f38ba8";
+    brightGreen = "#a6e3a1";
+    brightYellow = "#f9e2af";
+    brightBlue = "#89b4fa";
+    brightMagenta = "#f5c2e7";
+    brightCyan = "#94e2d5";
+    brightWhite = "#a6adc8";
+  };
+in
 {
   home.packages = with pkgs; [
     ttyd
@@ -21,7 +46,10 @@
       Environment = "LD_LIBRARY_PATH=${pkgs.libwebsockets}/lib:${pkgs.libuv}/lib";
       ExecStart = pkgs.writeShellScript "ttyd-start.sh" ''
         CREDENTIAL=$(<${config.sops.secrets."ttyd/credential".path})
-        exec ${pkgs.ttyd}/bin/ttyd -c "$CREDENTIAL" -p 3000 -W ${pkgs.zsh}/bin/zsh
+        exec ${pkgs.ttyd}/bin/ttyd \
+          -c "$CREDENTIAL" \
+          -t 'theme=${builtins.toJSON theme}' \
+          -p 3000 -W ${pkgs.zsh}/bin/zsh
       '';
       Restart = "always";
     };
