b3da2aa0db2b590dc35579783e719e36a63567bf
evf <eliseuv@pm.me>
2026-09-05 11:08:23 -0300

    Remove old configs dir

--- .old/qutebrowser/.config/qutebrowser/greasemonkey/nitter.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name           Twitter to Nitter redirector
-// @namespace      Custom userscripts
-// @match          https://twitter.com/*
-// @match          https://www.twitter.com/*
-// @run-at         document-start
-// ==/UserScript==
-
-location.href = location.href.replace("twitter.com", "nitter.net");
--- .old/qutebrowser/.config/qutebrowser/greasemonkey/wikiwand.js
+++
@@ -1,8 +0,0 @@
-// ==UserScript==
-// @name           Wikipedia to Wikiwand redirector
-// @namespace      Custom userscripts
-// @match          https://en.wikipedia.org/wiki/*
-// @run-at         document-start
-// ==/UserScript==
-
-location.href = location.href.replace("en.wikipedia.org/wiki", "wikiwand.com/en");
